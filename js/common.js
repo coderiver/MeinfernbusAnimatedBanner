f125619e315e99be2c8119e251994e30208068ae
mirantel <magi.mirantel@gmail.com>
2013-03-04 13:28:33 +0200

    js paused

--- js/common.js
+++ js/common.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
+	$('.bus .bus__close').click(function() {
+		$(this).parent('.bus').remove();
+	});
 
+	$('.bus').hover(
+		function () {
+			$(this).addClass('paused');
+		},
+		function () {
+			$(this).removeClass('paused');
+		}
+	);
 });
