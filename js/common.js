f3556aba49815c124f4b726855e9aebe1999d790
mirantel <magi.mirantel@gmail.com>
2013-03-06 13:02:54 +0200

    update

--- js/common.js
+++ js/common.js
@@ -6,9 +6,13 @@ $(document).ready(function() {
 	$('.bus').hover(
 		function () {
 			$(this).addClass('paused');
+			$(this).children().children('.bus__wheel1').addClass('paused');
+			$(this).children().children('.bus__wheel2').addClass('paused');
 		},
 		function () {
 			$(this).removeClass('paused');
+			$(this).children().children('.bus__wheel1').removeClass('paused');
+			$(this).children().children('.bus__wheel2').removeClass('paused');
 		}
 	);
 });
