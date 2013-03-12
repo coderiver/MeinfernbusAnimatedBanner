64f65faaeb72dc9bc57ae9895a8506ae3d17467c
mirantel <magi.mirantel@gmail.com>
2013-03-12 12:42:23 +0200

    hover

--- js/common.js
+++ js/common.js
@@ -2,17 +2,6 @@ $(document).ready(function() {
 	$('.bus .bus__close').click(function() {
 		$(this).parent('.bus').remove();
 	});
-
-	$('.bus').hover(
-		function () {
-			$(this).addClass('paused');
-			$(this).children().children('.bus__wheel1').addClass('paused');
-			$(this).children().children('.bus__wheel2').addClass('paused');
-		},
-		function () {
-			$(this).removeClass('paused');
-			$(this).children().children('.bus__wheel1').removeClass('paused');
-			$(this).children().children('.bus__wheel2').removeClass('paused');
-		}
-	);
+	function bus(){$('.bus').removeClass('bus_ride').addClass('bus_stay');}
+	setTimeout(bus, 10000)
 });
